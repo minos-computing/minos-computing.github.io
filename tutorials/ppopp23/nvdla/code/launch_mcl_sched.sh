4fe0e8b767149d45f7e617d6e6d75db4a47a1f1a
Ryan Friese <ryan.friese@pnnl.gov>
2023-02-22 08:23:10 +0000

    updates for ppopp23

---
+++ tutorials/ppopp23/nvdla/code/launch_mcl_sched.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+num_instances=`ls nvdla_emulator_configs/ | wc -l`
+devices=""
+
+for i in `seq 0 $(( num_instances-1 ))`; do
+	devices="${devices}nvdlaemu "
+	port=$(( 6000+i )) 
+	export POCL_NVDLAEMU${i}_PARAMETERS="127.0.0.1:${port}"
+done
+# printenv
+# echo "${devices}"
+POCL_DEVICES="${devices}" mcl_sched &
